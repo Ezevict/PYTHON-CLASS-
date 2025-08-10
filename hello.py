33207178285ae9a5b7ce3e3298a11d6a384bad03
Ezekiel <chidubemez3@gmail.com>
2025-08-10 16:12:01 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+# hello word
+#print - command to give output to the console
+print("Hello Ezekiel")
+
+if 5> 2:
+    print("5 is greater than 2")
+
+if (2< 5) :
+    print("2 is less than 5")
+
+""""""
+23456
+12345
+234
+""""""    
