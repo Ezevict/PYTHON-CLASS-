4b10117245d53fe6f8d9c529ab8a3e8bb0900cf8
Ezekiel <chidubemez3@gmail.com>
2025-08-17 20:11:40 +0100

    SimpleCalc

--- assignment/simplecalc.py
+++ assignment/simplecalc.py
@@ -1,21 +1,27 @@
 """
-This program demonstrates basic arithmetic operations using two variables.
-It performs addition, subtraction, multiplication, and division,
-and prints the result of each operation with a descriptive message.
+This program allows the user to choose an arithmetic operation to perform
+on two predefined numbers. Based on the user's choice, the program calculates and displays the result with a descriptive message.
 """
 
 # Define two variables
-num1 = int(input("enter the first number: "))
-num2 = int(input("enter the second number: "))
+num1 = 10
+num2 = 5
 
-# Perform operations
-sum_result = num1 + num2
-sub_result = num1 - num2
-mul_result = num1 * num2
-div_result = num1 / num2
+# Ask the user to choose an operation
+operation = input("Which arithmetic operation would you like to perform? (+, -, *, /): ")
 
-# Print results with descriptive text
-print("This is the sum of the two numbers:", sum_result)
-print("This is the difference of the two numbers:", sub_result)
-print("This is the product of the two numbers:", mul_result)
-print("This is the quotient of the two numbers:", div_result)
+# Perform the chosen operation using conditional statements
+if operation == '+':
+    result = num1 + num2
+    print("This is the sum of the two numbers:", result)
+elif operation == '-':
+    result = num1 - num2
+    print("This is the difference of the two numbers:", result)
+elif operation == '*':
+    result = num1 * num2
+    print("This is the product of the two numbers:", result)
+elif operation == '/':
+    result = num1 / num2
+    print("This is the quotient of the two numbers:", result)
+else:
+    print("Invalid operation selected. Please choose one of +, -, *, /.")
